a0a84f61ef1219b27c6473302e4f9c34e3962e82
Facundoo15 <fangulos1@upao.edu.pe>
2024-07-03 00:09:30 -0500

    feat: Correcion de CREDENCIALES

--- src/main/java/com/alexandertutoriales/ecommerce/service/config/WebConfig.java
+++ src/main/java/com/alexandertutoriales/ecommerce/service/config/WebConfig.java
@@ -9,8 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
+        registry.addMapping("/**").allowedOriginPatterns("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowCredentials(true);
     }
